ccde341398fe853e37bec1e0de10939e5606b2be
munisai <megamuni27@gmail.com>
2022-02-22 20:33:17 +0530

    init

--- Exercise/views.py
+++ Exercise/views.py
@@ -1,3 +1,126 @@
-from django.shortcuts import render
+from base64 import urlsafe_b64decode
+from email import message
+from lib2to3.pgen2.tokenize import generate_tokens
+from django.contrib.sites.shortcuts import get_current_site
+from django.shortcuts import redirect, render,HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from mega import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from . tokens import generate_token
 
-# Create your views here
+
+# Create your views here.
+def home(request):
+    return render(request,"Exercise/index.html")
+
+def signin(request):
+    
+    if request.method=="POST":
+        username=request.POST['username']
+        pass1=request.POST['pass1']
+        #return redirect('home')
+
+        user=authenticate(username=username,password=pass1)
+
+        if user is not None:
+            login(request,user)
+            fname=user.first_name
+            return render(request,"Exercise/index.html", {'fname':fname})
+        else:
+            messages.error(request,"Invalid Credtials")
+            return redirect('signin')
+
+    return render(request,"Exercise/signin.html")
+
+def signup(request):
+    if request.method=="POST":
+        username=request.POST['username']
+        fname=request.POST['fname']
+        lname=request.POST['lname']
+        email=request.POST['email']
+        pass1=request.POST['pass1']
+        pass2=request.POST['pass2']
+
+        if User.objects.filter(username=username):
+            messages.error(request,"username already exist")
+            return redirect('signup')
+
+        if User.objects.filter(email=email):
+            messages.error(request,"Email already exist")
+            return redirect('signup')
+        
+        if len(username)>10:
+            messages.error(request,"keep length of the username less than")
+            return redirect('signup')
+        
+        if len(username)<5:
+            messages.error(request,"username is too short")
+            return redirect('signup')
+
+        if pass1!=pass2:
+            messages.error(request,"passwords are different")
+            return redirect('signup')
+
+        if not username.isalnum():
+            messages.error(request,"username must be mix of numbers and alphabets")
+            return redirect('signup')
+
+        myuser=User.objects.create_user(username,email,pass1)
+        myuser.first_name=fname
+        myuser.last_name=lname
+        myuser.is_active =False
+
+        myuser.save()
+
+        messages.success(request,"your accout has been created successfully. we have sent an email please confirm it")
+
+        #WELCOME TO EMAIL
+
+        subject='email conformation'
+        message='hello'+myuser.first_name +"!! \n\n welcome to wholeness \n thank you for visitng our website \nplease conform your email address in order to activate your account.\n thanking you"
+
+        from_email=settings.EMAIL_HOST_USER
+        to_list=[myuser.email]
+        send_mail(subject,message,from_email,to_list,fail_silently=True)
+        
+        current_site=get_current_site(request)
+        
+        email_subject="confirm your email !!"
+        message2=render_to_string('email_conformation.html',{
+            'name':myuser.first_name,
+            'domain':current_site.domain,
+            'uid': urlsafe_base64_decode(force_bytes(myuser.pk)),
+            'token':generate_token.make_token(myuser)
+
+        })
+        email=message.EmailMessage(email_subject,message2,settings.EMAIL_HOST_USER,[myuser.email])
+
+
+        return redirect('signin')
+
+    return render(request,"Exercise/signup.html")
+
+def signout(request):
+    logout(request)
+    messages.success(request,"Logged out Successfully!")
+    return redirect('home')
+
+'''def activate(request, uidb64,token):
+    try:
+        #uid =force_text(urlsafe_base64_decode(uidb64))
+        myuser=User.onjects.get(pk=uid)
+    except (TypeError,ValueError,OverflowError,User.DoesNotExist):
+        myuser=None
+    
+    if myuser is not None and generate_token.check_token(myuser,token):
+        myuser.is_active=True
+        myuser.save()
+        login(request,myuser)
+        return redirect('home')
+    else:
+        render(request, 'activation_failed.html')'''
