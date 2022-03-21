566cd07e22f028b8161b4143f811ea63ad5b297b
munisai <megamuni27@gmail.com>
2022-03-21 19:05:20 +0530

    init

--- Exercise/models.py
+++ Exercise/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class name(models.Model):
-    first_name=models.CharField(max_length=100)
+    f_name=models.CharField(max_length=100)
     last_name=models.CharField(max_length=100)
 
     def __str__(self):
